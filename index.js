a07c7c4a3f1b09421f4c11997676cd247e820407
Alekc <khabirov_alekc@mail.ru>
2022-12-09 12:36:50 +0500

    добавил позицию

--- index.js
+++ index.js
@@ -8,3 +8,11 @@
     console.log(item);
     console.log('на сумму - ${counts*zchena}');
 }
+{
+    const item = 'milk';
+    const counts = 5;
+    const categoriesItem = 'products';
+    const zchena = 58;
+    
+}console.log(item);
+console.log('на сумму ${counts*zchena}');
