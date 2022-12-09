66e85d6c6a6224e62a057f548b2c3b1336442ee6
Alekc <khabirov_alekc@mail.ru>
2022-12-09 14:58:29 +0500

    перешёл в следующий модуль

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
     const categoriesItem ="products";
     const zchena = 27;
     console.log(item);
-    console.log('на сумму - ${counts*zchena}');
+    console.log(`на сумму - ${counts*zchena}`);
 }
 {
     const item = 'milk';
@@ -15,5 +15,8 @@
     const zchena = 58;
     
     console.log(item);
-    console.log('на сумму ${counts*zchena}');
+    console.log(`на сумму - ${counts*zchena}`);
 }
+const myName ='Алик';
+document.body.innerHTML=`<h1 class="title">Привет ${myName}</h1>
+<p>Сейчас ${new Date().getFullYear()} год</p>`;
