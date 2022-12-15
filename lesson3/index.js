6ae84f57631516581e784c03e4296cca501f2911
Alekc <khabirov_alekc@mail.ru>
2022-12-16 01:42:37 +0500

    снова

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,6 +1,20 @@
 'use strict'
 
 {
+    const item = prompt('Введите товар');
+    let count = +prompt('Введите количество товара');
+    const category = prompt('Введите категорию товара');
+    let price = +prompt('Введите цену товара'); 
+
+    console.log('item: ',typeof item);
+    console.log('count: ', typeof count);
+    console.log('category:', typeof category);
+    console.log('price: ', typeof price);
+
+    console.log('На складе', count, 'единиц товара', item, 'на сумму', (count*price), 'P');
+    }
+
+{
     const item = 'broad';
     const counts = 10;
     const categoriesItem ="products";
---
+++ lesson3/myJs.js
@@ -0,0 +1,6 @@
+'use strict';
+{
+const myWeight =67;
+const levelLight = 3e8;
+console.log('моё Е = ${myWeight*levelLight}');
+}
