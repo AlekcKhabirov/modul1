a7775eb61a4136ac6a6df026755eb29c7adb4a4b
Alekc <khabirov_alekc@mail.ru>
2022-12-16 01:55:06 +0500

    снова

--- lesson3/index.js
+++ lesson3/index.js
@@ -2,16 +2,16 @@
 
 {
     const item = prompt('Введите товар');
-    let count = +prompt('Введите количество товара');
+    let counts = +prompt('Введите количество товара');
     const category = prompt('Введите категорию товара');
     let price = +prompt('Введите цену товара'); 
 
     console.log('item: ',typeof item);
-    console.log('count: ', typeof count);
+    console.log('count: ', typeof counts);
     console.log('category:', typeof category);
     console.log('price: ', typeof price);
 
-    console.log('На складе', count, 'единиц товара', item, 'на сумму', (count*price), 'P');
+    console.log(`"В магазине" , counts, "единиц товара" , item, "на сумму", ${counts*price}, "P."`);
     }
 
 {
